35b1f4b8f69f9a8110446029af50d1277bbb86f3
Rocky Wei <finscn@gmail.com>
2017-10-02 00:19:33 +0800

    remove the argument `square`

--- src/CompositeRectTileLayer.ts
+++ src/CompositeRectTileLayer.ts
@@ -2,7 +2,7 @@
 
 module PIXI.tilemap {
     export class CompositeRectTileLayer extends PIXI.Container {
-        constructor(zIndex?: number, bitmaps?: Array<Texture>, useSquare?: boolean, texPerChild?: number) {
+        constructor(zIndex?: number, bitmaps?: Array<Texture>, texPerChild?: number) {
             super();
             this.initialize.apply(this, arguments);
         }
@@ -13,16 +13,14 @@ module PIXI.tilemap {
 
         z: number;
         zIndex: number;
-        useSquare: boolean;
         shadowColor = new Float32Array([0.0, 0.0, 0.0, 0.5]);
         texPerChild: number;
         modificationMarker = 0;
         _globalMat: PIXI.Matrix = null;
         _tempScale: Array<number> = null;
 
-        initialize(zIndex?: number, bitmaps?: Array<Texture>, useSquare?: boolean, texPerChild?: number) {
+        initialize(zIndex?: number, bitmaps?: Array<Texture>, texPerChild?: number) {
             this.z = this.zIndex = zIndex;
-            this.useSquare = useSquare;
             this.texPerChild = texPerChild || 16;
             if (bitmaps) {
                 this.setBitmaps(bitmaps);
@@ -135,7 +133,7 @@ module PIXI.tilemap {
 
         renderWebGL(renderer: WebGLRenderer) {
             var gl = renderer.gl;
-            var shader = renderer.plugins.tilemap.getShader(this.useSquare);
+            var shader = renderer.plugins.tilemap.getShader();
             renderer.setObjectRenderer(renderer.plugins.tilemap);
             renderer.bindShader(shader);
             //TODO: dont create new array, please
@@ -143,18 +141,11 @@ module PIXI.tilemap {
             renderer._activeRenderTarget.projectionMatrix.copy(this._globalMat).append(this.worldTransform);
             shader.uniforms.projectionMatrix = this._globalMat.toArray(true);
             shader.uniforms.shadowColor = this.shadowColor;
-            if (this.useSquare) {
-                var tempScale = this._tempScale = (this._tempScale || [0, 0]);
-                tempScale[0] = this._globalMat.a >= 0 ? 1 : -1;
-                tempScale[1] = this._globalMat.d < 0 ? 1 : -1;
-                var ps = shader.uniforms.pointScale = tempScale;
-                shader.uniforms.projectionScale = Math.abs(this.worldTransform.a) * renderer.resolution;
-            }
             var af = shader.uniforms.animationFrame = renderer.plugins.tilemap.tileAnim;
             //shader.syncUniform(shader.uniforms.animationFrame);
             var layers = this.children;
             for (var i = 0; i < layers.length; i++)
-                (layers[i] as RectTileLayer).renderWebGL(renderer, this.useSquare);
+                (layers[i] as RectTileLayer).renderWebGL(renderer);
         }
 
         isModified(anim: boolean) {
