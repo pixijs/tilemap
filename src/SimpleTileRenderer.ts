fc291a0a39f086bfc966eea09258134448e9689e
Rocky Wei <finscn@gmail.com>
2017-10-05 02:26:21 +0800

    bindTexture with texture(not baseTexture)

--- src/SimpleTileRenderer.ts
+++ src/SimpleTileRenderer.ts
@@ -40,13 +40,11 @@ module PIXI.tilemap {
                     continue;
                 }
 
-                const baseTexture = texture.baseTexture;
-
-                this.texLoc[0] = renderer.bindTexture(baseTexture, 0, true);
+                this.texLoc[0] = renderer.bindTexture(texture, 0, true);
                 shader.uniforms.uSamplers = this.texLoc;
 
-                this.samplerSize[0] = 1.0 / baseTexture.width;
-                this.samplerSize[1] = 1.0 / baseTexture.height;
+                this.samplerSize[0] = 1.0 / texture.baseTexture.width;
+                this.samplerSize[1] = 1.0 / texture.baseTexture.height;
                 shader.uniforms.uSamplerSize = this.samplerSize;
 
                 break;
